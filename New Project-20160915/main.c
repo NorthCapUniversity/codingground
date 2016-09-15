d8ab585192cd635231f5747890bda79d6730c139
NorthCapUniversity <NorthCapUniversity@users.noreply.github.com>
2016-09-15 20:30:21 +0530

    Created by Coding Ground Client

---
+++ New Project-20160915/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    char f[6]="saloni";
+    
+    int i;
+    printf("Hello, World!\n");
+    printf("%s",f);
+    printf("\n");
+    for(i=0;i<6;i++)
+    {
+         printf("%d\t",f[i]);
+    }
+    return 0;
+}
+
+
